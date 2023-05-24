3aa6dfb6e62316ed4e5b167996f8f7e6625003c8
b9348 <41979647+b9348@users.noreply.github.com>
2023-05-24 12:35:22 +0800

    Update list.js

--- list.js
+++ list.js
@@ -3,16 +3,19 @@ export const list = [
     id: 1,
     name: "hhh",
     num: 7453,
+    img:'https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL2kvcyFBczVoZ0w4dTlPMzFoMHE0MDJONkVjanVCbnl1P2U9MnptVFcz.jpg'
   },
   {
     id: 2,
     name: "qwewq",
     num: 965201,
+    img:'https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL2kvcyFBczVoZ0w4dTlPMzFoMHZrcUFocVktX3dCV2pKP2U9YnhGUlB6.jpg'
   },
   {
     id: 3,
     name: "2werfgh",
     num: 36914,
+    img:'https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL2kvcyFBczVoZ0w4dTlPMzFpQktLMWN5WVdPR00yejVVP2U9M0JzMWNE.jpg'
   },
   {
     id: 4,
