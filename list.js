b9db5a013bc5b792786f05c4f8adbab1443ab0fd
b9348 <41979647+b9348@users.noreply.github.com>
2023-05-24 13:11:33 +0800

    Update list.js

--- list.js
+++ list.js
@@ -1,25 +1,26 @@
 export const list = [
   {
     id: 1,
-    name: "hhh",
+    name: "嗨嗨嗨",
     num: 7453,
     img:'https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL2kvcyFBczVoZ0w4dTlPMzFoMHE0MDJONkVjanVCbnl1P2U9MnptVFcz.jpg'
   },
   {
     id: 2,
-    name: "qwewq",
+    name: "小亮",
     num: 965201,
     img:'https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL2kvcyFBczVoZ0w4dTlPMzFoMHZrcUFocVktX3dCV2pKP2U9YnhGUlB6.jpg'
   },
   {
     id: 3,
-    name: "2werfgh",
+    name: "我他妈直接忽略",
     num: 36914,
     img:'https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL2kvcyFBczVoZ0w4dTlPMzFpQktLMWN5WVdPR00yejVVP2U9M0JzMWNE.jpg'
   },
   {
     id: 4,
-    name: "zzxc",
+    name: "本拉登",
     num: 252080124,
+    img:'https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBZ204d3BjSVhDanNnNHRuV2VyWDNxWk9BM0JBcUE=.jpg'
   },
 ];
